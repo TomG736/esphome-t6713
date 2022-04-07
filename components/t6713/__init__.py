59875eeb71e1efe9ac39c91abd7d7111bf193013
tgrant <tom.grant760@gmail.com>
2022-04-07 22:59:53 +0100

    remove commented schema

--- components/t6713/__init__.py
+++ components/t6713/__init__.py
@@ -1,29 +1,3 @@
-# import esphome.codegen as cg
-# import esphome.config_validation as cv
-# from esphome.components import i2c
-# from esphome.const import CONF_ID, CONF_SLEEP_DURATION
-
-# DEPENDENCIES = ['i2c']
-
-# CONF_I2C_ADDR = 0x51
-
-# t6713 = cg.esphome_ns.namespace('t6713')
-# T6713 = t6713.class_('T6713', cg.Component, i2c.I2CDevice)
-
-# CONFIG_SCHEMA = cv.Schema({
-#     cv.GenerateID(): cv.declare_id(T6713),
-#     cv.Optional(CONF_SLEEP_DURATION): cv.positive_time_period_seconds,
-# }).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(CONF_I2C_ADDR))
-
-# def to_code(config):
-#     var = cg.new_Pvariable(config[CONF_ID])
-#     if CONF_SLEEP_DURATION in config:
-#         cg.add(var.set_sleep_duration(config[CONF_SLEEP_DURATION]))
-#     yield cg.register_component(var, config)
-#     yield i2c.register_i2c_device(var, config)
-    
-
-  
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
